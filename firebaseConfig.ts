2df925bb74d17fb301032353dc09cde823db4064
vinirlmarques <viniciusrlmarques@gmail.com>
2025-05-10 14:43:43 -0300

    env

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,15 +3,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Configurações do Firebase (obtidas do seu exemplo)
+// Configurações do Firebase a partir das variáveis de ambiente
 const firebaseConfig = {
-  apiKey: "AIzaSyDBWIcjeKT85m3fs5AyX2biFYSXNqgrJ7E",
-  authDomain: "mayapostas.firebaseapp.com",
-  projectId: "mayapostas",
-  storageBucket: "mayapostas.firebasestorage.app",
-  messagingSenderId: "32246507754",
-  appId: "1:32246507754:web:d91d97b2cadb816c0bcf93",
-  measurementId: "G-90JJEYH4MS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializando o Firebase (evitando inicialização duplicada)
